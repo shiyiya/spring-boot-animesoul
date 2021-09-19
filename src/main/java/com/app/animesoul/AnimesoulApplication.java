845bb71c5963cec0294277638361d774b41c4ffb
By Lin <eleven_g11@qq.com>
2021-09-19 17:04:20 +0800

    Init Project

---
+++ src/main/java/com/app/animesoul/AnimesoulApplication.java
@@ -0,0 +1,13 @@
+package com.app.animesoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeSoulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeSoulApplication.class, args);
+    }
+
+}
